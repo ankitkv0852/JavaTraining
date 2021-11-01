dec5a0cf6f5de3b39efafe2c98fe2af5dec7ec54
ankitkv0852 <43729235+ankitkv0852@users.noreply.github.com>
2021-11-01 12:55:12 +0530

    Add files via upload

---
+++ TestContinent.java
@@ -0,0 +1,41 @@
+package com.TestCase;
+
+import java.util.*;
+
+
+class FlowerDemo1
+{
+	public void displayFlower(String continent,HashMap<String,String> m)
+	{
+		if(continent.equalsIgnoreCase("asia"))
+		{
+			System.out.println(m.get(continent));
+		}
+		else if(continent.equalsIgnoreCase("europe"))
+		{
+			System.out.println(m.get(continent));
+		}
+		else if(continent.equalsIgnoreCase("africa"))
+		{
+			System.out.println(m.get(continent));
+		}	
+	}	
+}
+
+public class TestContinent {
+
+	public static void main(String[] args) 
+	{
+		HashMap<String,String> map= new HashMap<>();
+		map.put("ASIA","{INDIA: LOTUS, PAKISTAN: JASMINE, NEPAL:LALI GURANS}");
+		map.put("EUROPE","{GERMANY:CORN FLOWER, FRANCE:LILY, GREECE: ACANTHUS MOLLIS}");
+		map.put("AFRICA","{EGYPT: BLUE LOTUS, NIGERIA: YELLOW TRUMPET, KENYA: ORCHID}");
+		Scanner sc=new Scanner(System.in);
+		FlowerDemo1 f= new FlowerDemo1();
+		System.out.println("Choose a continent: ASIA, EUROPE or AFRICA");
+		String continent=(sc.nextLine()).toUpperCase();
+		f.displayFlower(continent,map);
+		sc.close();
+	}
+
+}
